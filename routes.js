9940ad1f882d9287ee3f325f91b4689ef0015bd3
TaryNascimento <juju2004.tary@gmail.com>
2024-04-26 15:10:51 -0300

    Configurando rotas

--- routes.js
+++ routes.js
@@ -1,12 +1,14 @@
 // routes.js
 const express = require('express');
 const router = express.Router();
-const controller = require('./controller');
+const controller = require('./Controller/controller');
 
+// Rota raiz para exibir uma mensagem de boas-vindas
 router.get('/', (req, res) => {
     res.send('Bem-vindo à API do Currículo!');
 });
 
+// Rota para manipular as requisições relacionadas ao currículo
 router.get('/curriculo', controller.getCurriculo);
 router.post('/criarCurriculo', controller.createCurriculo);
 
