f9b654e444b1bb6e59e694b4c628a277c77034dc
TaryNascimento <tary.junior47@gmail.com>
2024-04-26 09:54:50 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./db');
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
