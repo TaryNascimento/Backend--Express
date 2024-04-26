13bb546dcaca3db92988677456537894d1764c04
TaryNascimento <juju2004.tary@gmail.com>
2024-04-26 14:49:55 -0300

    Testando

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', routes); // Use as rotas definidas no arquivo routes.js
+app.use('/', routes); // Usando as rotas definidas no arquivo routes.js
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+export default app; // Exportando o app para uso em outros arquivos
