651166ddbb71a93a8b1710aa8a09fdde33eeb642
TaryNascimento <juju2004.tary@gmail.com>
2024-04-26 15:34:33 -0300

    ...

--- routes.js
+++ routes.js
@@ -4,7 +4,8 @@ const controller = require('./controller');
 
 router.get('/', (req, res) => {
     res.send('Bem-vindo à API do Currículo!');
-  });
+});
+
 router.get('/curriculo', controller.getCurriculo);
 router.post('/criarCurriculo', controller.createCurriculo);
 router.delete('/deletarCurriculo/:id', controller.deleteCurriculo);
